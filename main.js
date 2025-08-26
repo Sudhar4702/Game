9540f4a1e4131ca749d847fe5b50131b35286c8b
Sudharsan M <sudhar4702@gmail.com>
2025-08-27 01:10:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const custom_play=document.querySelector(".icon-1-play");
 const play_with_ai=document.querySelector(".icon-2-play");
 
 custom_play.addEventListener("click",(e)=>{
-    window.location.href = 'customplay.html';
+    window.location.href = 'https://Sudhar4702.github.io/Game/customplay.html';
 });
 
 play_with_ai.addEventListener("click",(e)=>{
-    window.location.href = 'challengewithai.html';
+    window.location.href = 'https://Sudhar4702.github.io/Game/challengewithai.html';
 
 });
+
