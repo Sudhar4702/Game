2727c49357b16fd99d49d8bcac1701dd3e98da01
Sudharsan M <sudhar4702@gmail.com>
2025-08-27 01:06:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const custom_play=document.querySelector(".icon-1-play");
 const play_with_ai=document.querySelector(".icon-2-play");
 
 custom_play.addEventListener("click",(e)=>{
-    window.location.href = 'http://127.0.0.1:5500/chess/customplay.html';
+    window.location.href = 'customplay.html';
 });
 
 play_with_ai.addEventListener("click",(e)=>{
-    window.location.href = 'http://127.0.0.1:5500/chess/challengewithai.html';
+    window.location.href = 'challengewithai.html';
+
 });
